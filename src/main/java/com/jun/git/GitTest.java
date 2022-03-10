8bd8e8abc0b78083074be1cc68571de21075959b
眸中客J <48201632+zjGenius@users.noreply.github.com>
2022-03-10 13:34:33 +0800

    github commit

--- src/main/java/com/jun/git/GitTest.java
+++ src/main/java/com/jun/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("push test!");
         System.out.println("pull test!");
         System.out.println("gitee pull test");
+        System.out.println("github pull test");
     }
 }
