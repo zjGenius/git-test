06618f1ef6fde04a27748471442061c57907d1db
Jun <1905212905@qq.com>
2022-03-10 12:10:13 +0800

    first commit

---
+++ src/main/java/com/jun/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.jun.git;
+
+/**
+ * @program: git-test
+ * @description:
+ * @author: Jun
+ * @create: 2022-03-10 12:08
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git!");
+    }
+}
