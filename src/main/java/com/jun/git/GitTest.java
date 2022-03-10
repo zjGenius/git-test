d72771b849661ba700c0865a997e4943fe078fb1
Jun <1905212905@qq.com>
2022-03-10 12:16:13 +0800

    second commit

--- src/main/java/com/jun/git/GitTest.java
+++ src/main/java/com/jun/git/GitTest.java
@@ -9,5 +9,6 @@ package com.jun.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git!");
+        System.out.println("hello Git2!");
     }
 }
